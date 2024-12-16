dceca8ada48f4f0f6e8fe50ac90ba43142935ff3
Goldie Ornstein <goldieorn@gmail.com>
2024-12-15 21:42:41 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-background text-foreground p-8">
+      <h1 className="text-4xl font-bold">Book It Now</h1>
+    </div>
+  );
+}
